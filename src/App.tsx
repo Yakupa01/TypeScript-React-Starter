baf4af95f885489bb64feaea71984aa1ad70eded
Yakupa01 <mkruquliyakupha3@gmail.com>
2026-04-17 08:59:51 +0200

    Refactor App component to use MUI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import * as React from 'react';
-import './App.css';
-import Hello from './components/Hello';
-
-const logo = require('./logo.svg');
+import { Typography, Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <Hello name="TypeScript" />
-    </div>
+    <Container>
+      <Typography variant="h3">
+        Hello World
+      </Typography>
+    </Container>
   );
 }
 
 export default App;
+
 
